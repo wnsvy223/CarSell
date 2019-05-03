8527cf4d4102f3645fa38e5879cf806e5f9e3e96
wnsvy <wnsvy223@naver.com>
2019-05-04 00:45:09 +0900

    reply logic

--- routes/board-free.js
+++ routes/board-free.js
@@ -4,6 +4,7 @@ var mysqlDB = require('../mysqlDB');
 var moment = require('moment');
 
 var documentNum = 0; //게시물 번호값 받을 변수 get detail 라우터를 통해 본문 진입시 받아옴. 
+var profileImage_w = '';
 var now = moment().format('YYYY-MM-DD HH:mm:ss:SSS');
 
 router.get('/', function(req, res, next) {
@@ -20,8 +21,8 @@ router.get('/', function(req, res, next) {
                 connection.query(query,function (err, rows, fields){
                     if(err){
                         console.log('query error'+err);
-                    }else{               
-                        console.log('전체게시물 : ' + rows.length);
+                    }else{              
+                        //console.log('전체게시물 : ' + rows.length);
                         var renderParam ={
                             email : req.session.email, 
                             profileImage : req.session.userProfile, 
@@ -65,7 +66,7 @@ router.get('/page/:idx', function(req, res, next){
                             if(err){
                                 console.log('query error'+err);
                             }else{
-                                console.log('페이지:' + currentPage + '페이지 인덱스:' + parseInt(req.params.idx));
+                                //console.log('페이지:' + currentPage + '페이지 인덱스:' + parseInt(req.params.idx));
                                 if(rows.length < 10){ // 페이지에 남은 게시물이 10개 미만일 경우
                                     var renderParam ={
                                         email : req.session.email, 
@@ -163,7 +164,6 @@ router.post('/write/reply', function(req, res, next){
         if(req.body.content_reply == ''){
             res.send('<script type="text/javascript">alert("내용을 입력해 주세요");</script>');
         }else{
-       
             var reply_param = {
                 board_num: documentNum,
                 userId_reply: req.session.userId,
@@ -171,7 +171,7 @@ router.post('/write/reply', function(req, res, next){
                 timeStamp : now
             };
            
-            var query = 'insert into board_reply (board_num, userId_reply, reply_content, timeStamp) values(?,?,?,?)';
+            var query = 'insert into board_reply (board_num, userId_reply, reply_content, timeStamp_reply) values(?,?,?,?)';
             mysqlDB.getConnection(function(err, connection){
                 if(err){
                     console.log('connection pool error'+err);
@@ -180,8 +180,8 @@ router.post('/write/reply', function(req, res, next){
                         if(err){
                             console.log('quey error'+err);
                         }else{
-                            console.log('댓글작성완료');
-                            res.redirect('/board-free');
+                            var url = '/board-free/detail?index=' + documentNum + '&userProfile_w='+profileImage_w;
+                            res.redirect(url);
                             connection.release();
                         }
                     });
@@ -197,39 +197,54 @@ router.get('/detail?:data_board_num', function(req, res, next){
         res.render('index'); 
     }else{
         // 뷰에서 href를 통해 넘겨준 시멘틱URL 값에 해당 게시물의 번호가 날아옴.
-        console.log('시멘틱URL 인덱스 : ' + req.query.index);
+        // console.log('시멘틱URL 인덱스 : ' + req.query.index);
         documentNum = req.query.index;
+        profileImage_w = req.query.userProfile_w;
         mysqlDB.getConnection(function(err, connection){
             if(err){
                 console.log('connection pool error'+err);
             }else{
-                var query = 'select * from board where board_num=?';
+                var query = 'select * from board where board.board_num=?';
+            
                 connection.query(query, [req.query.index], function(err, rows, fields){
                     if(err){
                         console.log('quey error'+err);
-                    }else{                   
-                        var renderParam = {
-                            email : req.session.email, 
-                            profileImage : req.session.userProfile, 
-                            userId : req.session.userId,
-                            userProfile_w : req.query.userProfile_w, // 작성자 프로필사진
-                            board_title : rows[0].board_title,
-                            userId_w : rows[0].userId_w,
-                            timeStamp : rows[0].timeStamp,
-                            content : rows[0].content,
-                            rows: rows // 본문 조회값
-                        };
-                    
-                        var board_idx = rows[0].board_num; // 쿼리된 번호에 해당하는 게시글의 조회수 +1 해서 업데이트
-                        var updateViewsQuery = 'update board set board.readCount=board.readCount + 1 where board.board_num=?';
-                        connection.query(updateViewsQuery,[board_idx], function(err, rows, fields){
+                    }else{     
+                               
+                        var select_reply = 'select * from board_reply where board_num=?'
+                        connection.query(select_reply, [documentNum], function(err, subrows, fields){
                             if(err){
                                 console.log('quey error'+err);
                             }else{
-                                res.render('board-free-detail', renderParam);
-                                connection.release(); 
+                                var renderParam = {
+                                    email : req.session.email, 
+                                    profileImage : req.session.userProfile, 
+                                    userId : req.session.userId,
+                                    userProfile_w : req.query.userProfile_w, // 작성자 프로필사진
+                                    board_title : rows[0].board_title,
+                                    userId_w : rows[0].userId_w,
+                                    timeStamp : rows[0].timeStamp,
+                                    content : rows[0].content,
+                                    moment: moment,
+                                    rows: rows, // 본문 조회값
+                                    reply: subrows, // 댓글 조회값
+                                    replyLength : subrows.length, // 댓글 갯수
+                                    
+                                };
+                               
+                                var board_idx = rows[0].board_num; // 쿼리된 번호에 해당하는 게시글의 조회수 +1 해서 업데이트
+                                //console.log('인덱스', + board_idx);
+                                var updateViewsQuery = 'update board set board.readCount=board.readCount + 1 where board.board_num=?';
+                                connection.query(updateViewsQuery,[board_idx], function(err, rows, fields){
+                                    if(err){
+                                        console.log('quey error'+err);
+                                    }else{
+                                        res.render('board-free-detail', renderParam);
+                                        connection.release(); 
+                                    }
+                                });
                             }
-                        });
+                        });  
                                         
                     }
                 });
